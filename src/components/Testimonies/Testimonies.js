6194b3cb8816c15a4cdc0f0829077223bbca307f
Joaquin Gavernet <joaquingavernet@gmail.com>
2021-08-22 22:14:01 -0300

    Trying to implement Swiper JavaScript.

--- src/components/Testimonies/Testimonies.js
+++ src/components/Testimonies/Testimonies.js
@@ -5,6 +5,12 @@ import React from 'react';
 import './Testimonies.scss';
 
 
+// Swiper
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Swiper Styles
+import 'swiper/swiper.scss';
+
 const Testimonies = () => {
 
 
@@ -12,7 +18,7 @@ const Testimonies = () => {
     <section className='home__testimonies'>
       <h3 className='testimonies__title'>Nuestros parceros confian en nuestro trabajo</h3>
       <p className='testimonies__text'>Vea lo que nuestros clientes hablan de nosotros</p>
-      <div className='testimonies__boxes'>
+      {/* <div className='testimonies__boxes'>
         <div className='boxes'>
           <img src='' alt='Comillas' className='boxes__img' />
           <p className='boxes__text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur sapien eget ex pharetra, vitae iaculis elit scelerisque. Duis sit amet finibus nibh, at ornare tellus.</p>
@@ -31,7 +37,35 @@ const Testimonies = () => {
           <h6 className='boxes__title'>Lorem ipsum |</h6>
           <img src='' alt='Logo' className='boxes__logo' />
         </div>
-      </div>
+      </div> */}
+
+      <Swiper
+        slidesPerView={3}
+        loop={true}
+        // spaceBetween={50}
+        // pagination={{ "clickable": true }}
+        // navigation={true}
+        className=''
+      >
+        <SwiperSlide className='example'>
+          <img src='' alt='Comillas' className='boxes__img' />
+          <p className='boxes__text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur sapien eget ex pharetra, vitae iaculis elit scelerisque. Duis sit amet finibus nibh, at ornare tellus.</p>
+          <h6 className='boxes__title'>Lorem ipsum |</h6>
+          <img src='' alt='Logo' className='boxes__logo' />
+        </SwiperSlide>
+        <SwiperSlide className=''>
+          <img src='' alt='Comillas' className='boxes__img' />
+          <p className='boxes__text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur sapien eget ex pharetra, vitae iaculis elit scelerisque. Duis sit amet finibus nibh, at ornare tellus.</p>
+          <h6 className='boxes__title'>Lorem ipsum |</h6>
+          <img src='' alt='Logo' className='boxes__logo' />
+        </SwiperSlide>
+        <SwiperSlide className=''>
+          <img src='' alt='Comillas' className='boxes__img' />
+          <p className='boxes__text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur sapien eget ex pharetra, vitae iaculis elit scelerisque. Duis sit amet finibus nibh, at ornare tellus.</p>
+          <h6 className='boxes__title'>Lorem ipsum |</h6>
+          <img src='' alt='Logo' className='boxes__logo' />
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 }
